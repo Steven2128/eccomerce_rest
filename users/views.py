41eda7875c1a673bd574c7913e57a713bb2e0ecc
Stephen Alarcón <slj212885@gmail.com>
2021-11-08 13:34:10 -0500

    Creando usuario con @api_view

--- users/views.py
+++ users/views.py
@@ -6,9 +6,15 @@ from .serializers import UserSerializer
 from .models import User
 
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def user_list_view(request):
     if request.method == 'GET':
         queryset = User.objects.all()
         serializer = UserSerializer(queryset, many=True)
         return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors)

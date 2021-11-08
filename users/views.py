1990269d26c9ffc28b0abc18d82f593059c1693a
Stephen Alarcón <slj212885@gmail.com>
2021-11-08 17:06:57 -0500

    Agregardo status a los Response

--- users/views.py
+++ users/views.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
@@ -11,29 +12,30 @@ def user_list_view(request):
     if request.method == 'GET':
         queryset = User.objects.all()
         serializer = UserSerializer(queryset, many=True)
-        return Response(serializer.data)
+        return Response(serializer.data, status=status.HTTP_200_OK)
     elif request.method == 'POST':
         serializer = UserSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(['GET', 'PUT', 'DELETE'])
 def user_detail_view(request, pk=None):
-    if request.method == 'GET':
-        if pk is not None:
-            queryset = User.objects.filter(pk=pk).first()
+    queryset = User.objects.filter(pk=pk).first()
+    if queryset:
+        if request.method == 'GET':
             serializer = UserSerializer(queryset)
-            return Response(serializer.data)
-    elif request.method == 'PUT':
-        queryset = User.objects.filter(pk=pk).first()
-        serializer = UserSerializer(queryset, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors)
-    elif request.method == 'DELETE':
-        queryset = User.objects.filter(pk=pk).first().delete()
-        return Response("Eliminado!")
-    
+            return Response(serializer.data, status=status.HTTP_200_OK)
+
+        elif request.method == 'PUT':
+            serializer = UserSerializer(queryset, data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data, status=status.HTTP_200_OK)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+        elif request.method == 'DELETE':
+            queryset.delete()
+            return Response({'message': 'Usuario eliminado correctamente!'}, status=status.HTTP_200_OK)
+    return Response({'message': 'No se ha encontrado ese usuario'}, status=status.HTTP_400_BAD_REQUEST)

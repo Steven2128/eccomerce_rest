648f7d14803a02e684d7c75d4e71ff02d5334ccb
Stephen Alarcón <slj212885@gmail.com>
2021-11-08 16:25:18 -0500

    Detail, update and delete de usuario con @api_view

--- users/urls.py
+++ users/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', user_list_view, name='usuario_list')
+    path('', user_list_view, name='usuario_list'),
+    path('<int:pk>/', user_detail_view, name='usuario_detail'),
 ]
--- users/views.py
+++ users/views.py
@@ -18,3 +18,22 @@ def user_list_view(request):
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors)
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def user_detail_view(request, pk=None):
+    if request.method == 'GET':
+        if pk is not None:
+            queryset = User.objects.filter(pk=pk).first()
+            serializer = UserSerializer(queryset)
+            return Response(serializer.data)
+    elif request.method == 'PUT':
+        queryset = User.objects.filter(pk=pk).first()
+        serializer = UserSerializer(queryset, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors)
+    elif request.method == 'DELETE':
+        queryset = User.objects.filter(pk=pk).first().delete()
+        return Response("Eliminado!")
+    
